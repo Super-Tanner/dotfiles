e41c684b5e61a40c6995b13c67390805d5faacbb
Tanner Beam <tanner@superconductive.com>
2022-07-12 18:36:03 -0700

    fix syntax error in

--- path_rebuild.sh
+++ path_rebuild.sh
@@ -1,2 +1,2 @@
 export PATH=""
-export PATH=/Users/beamt/.pyenv/shims:/Users/beamt/bin:/Users/beamt/.local/bin/usr/local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/bin:/usr/sbin:/bin:/sbin:
+export PATH=/Users/beamt/.pyenv/shims:/Users/beamt/bin:/Users/beamt/.local/bin:/usr/local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/bin:/usr/sbin:/bin:/sbin:
